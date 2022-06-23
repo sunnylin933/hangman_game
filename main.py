64492dc02e96749140bc539d37dba81fd3b0aedf
sunnylin933 <sl9128@nyu.edu>
2022-06-22 22:09:41 -0700

    extended possible words

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import random
 import art
-from word_lists import food_list, city_list
+from word_lists import valid_letters, food_list, city_list
 
+genre_chosen = False
 playing = True
 current_stage = 6
 guessed_letters = []
 incorrect_letters = []
 
-game_type_response = input("Welcome to Hangman! Please pick from one of the following categories: Foods, Cities ").lower()
-if any(game_type_response == response for response in ['foods', 'food', 'f', '0']):
-    game_type = food_list
-elif any(game_type_response == response for response in ['cities', 'city', 'c', '1']):
-    game_type = city_list
+print(art.logo)
+game_type_response = print("\nWelcome to Hangman!")
+
+#Player Genre Choice
+while(not genre_chosen):
+    game_type_response = input("Please pick from one of the following categories: Foods, Cities").lower()
+    genre_chosen = True
+    if any(game_type_response == response for response in ['foods', 'food', 'f', '0']):
+        game_type = food_list
+        print("You selected foods!")
+    elif any(game_type_response == response for response in ['cities', 'city', 'c', '1']):
+        game_type = city_list
+        print("You selected cities!")
+    else:
+        print("Invalid Response.")
+        genre_chosen = False
 
 chosen_word = random.choice(game_type) 
 word_state = ["_"] * len(chosen_word)
 
-#Removes spaces
+#Player Letter Choice
+##Removes spaces
 if(" " in chosen_word):
     for i in range(len(chosen_word)):
         if(chosen_word [i] == " "):
             word_state[i] = " "
 
+##Checks choice
 while(playing):
     print(art.stages[current_stage])
     if(not "_" in word_state):
@@ -32,15 +46,17 @@ while(playing):
         print("You Lose! Your word was: " + chosen_word)
     else:
         print("".join(word_state))
+
         guessed_letters_string = ", ".join(guessed_letters)
         print(f"\nGuessed Letters: {guessed_letters_string}")
+
         guessed_letter = input("Choose a letter: ").lower()
 
-        if(guessed_letter == " " or len(guessed_letter) > 1):
+        if(not guessed_letter in valid_letters or len(guessed_letter) > 1): #checks to see if guess is valid
             print("Invalid Response.")
-        elif(guessed_letter in guessed_letters):
+        elif(guessed_letter in guessed_letters): #checks to see if guess has already been guessed
             print("Letter already guessed.")
-        elif(guessed_letter != " " and guessed_letter in chosen_word):
+        elif(guessed_letter != " " and guessed_letter in chosen_word): #checks to see if guess is in chosen word
             for i in range(len(chosen_word)):
                 if chosen_word[i] == guessed_letter:
                     word_state[i] = guessed_letter  
--- word_lists.py
+++ word_lists.py
@@ -1,3 +1,8 @@
-food_list = ["boba milk tea", "fried chicken", "cheesecake", "tofu pudding", "cheeseburger", "dumplings", "pineapple", "tonkatsu ramen", 
-"chicken katsu", "fish taco", "waffle fries", "honey pancakes", "seasame bagel"]
-city_list = ["london", "boston", "shanghai", "beijing", "ho chi minh city", "taipei"]
+valid_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+food_list = ["boba milk tea", "fried chicken", "cheesecake", "tofu pudding", "cheeseburger", "dumplings", "pineapple", "tonkatsu ramen", "chicken katsu", "fish taco", 
+"waffle fries", "pancakes", "seasame bagel", "steak and fries", "seafood paella", "poutine", "french toast", "chicken noodle soup", "chicken parm", "meatball sub", 
+"fish and chips", "beef lasagna", "buttered lobster", "corn on a cob", "vanilla ice cream", "chocolate ice cream", "margherita pizza", "frozen yogurt", 
+"chocolate fudge", "apple pie", "macarons", "spring rolls", "grilled cheese", "cheese fondue", "strawberry milkshake"]
+city_list = ["london", "boston", "shanghai", "beijing", "taipei", "new york city", "singapore", "chicago", "seoul", "paris", "cairo", "ho chi minh city", "baghdad", "hong kong", 
+"rio de janeiro", "sydney", "los angeles", "madrid", "rome", "tokyo", "new orleans", "moscow"]
