4a6a5b61ad9c483f8ec290e967667921c9c3a35b
sunnylin933 <sl9128@nyu.edu>
2022-06-22 18:25:34 -0700

    art_expansion

--- main.py
+++ main.py
@@ -1,66 +1,11 @@
 import random
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import art
+from word_lists import food_list, city_list
+
 playing = True
 current_stage = 6
+guessed_letters = []
 incorrect_letters = []
-food_list = ["boba milk tea", "fried chicken", "cheesecake", "tofu pudding", "cheeseburger", "dumplings", "pineapple", "tonkatsu ramen", 
-"chicken katsu", "fish taco", "waffle fries", "honey pancakes", "seasame bagel"]
-city_list = ["london", "boston", "shanghai", "beijing", "ho chi minh city", "taipei"]
 
 game_type_response = input("Welcome to Hangman! Please pick from one of the following categories: Foods, Cities ").lower()
 if any(game_type_response == response for response in ['foods', 'food', 'f', '0']):
@@ -78,7 +23,7 @@ if(" " in chosen_word):
             word_state[i] = " "
 
 while(playing):
-    print(stages[current_stage])
+    print(art.stages[current_stage])
     if(not "_" in word_state):
         playing = False
         print("You Win! Your word was: " + chosen_word)
@@ -87,17 +32,22 @@ while(playing):
         print("You Lose! Your word was: " + chosen_word)
     else:
         print("".join(word_state))
-        guessed_letter = input("\nGuess a letter!").lower()
+        guessed_letters_string = ", ".join(guessed_letters)
+        print(f"\nGuessed Letters: {guessed_letters_string}")
+        guessed_letter = input("Choose a letter: ").lower()
 
         if(guessed_letter == " " or len(guessed_letter) > 1):
             print("Invalid Response.")
+        elif(guessed_letter in guessed_letters):
+            print("Letter already guessed.")
         elif(guessed_letter != " " and guessed_letter in chosen_word):
             for i in range(len(chosen_word)):
                 if chosen_word[i] == guessed_letter:
-                    word_state[i] = guessed_letter
+                    word_state[i] = guessed_letter  
+            guessed_letters += guessed_letter
             print("Correct Guess!\n")
         else:
             current_stage -= 1
             incorrect_letters += guessed_letter
             incorrect_letters_string = ", ".join(incorrect_letters)
-            print(f"Incorrect Guess! Incorrect letters so far: {incorrect_letters_string}\n")
+            print(f"Incorrect Guess!")
---
+++ word_lists.py
@@ -0,0 +1,3 @@
+food_list = ["boba milk tea", "fried chicken", "cheesecake", "tofu pudding", "cheeseburger", "dumplings", "pineapple", "tonkatsu ramen", 
+"chicken katsu", "fish taco", "waffle fries", "honey pancakes", "seasame bagel"]
+city_list = ["london", "boston", "shanghai", "beijing", "ho chi minh city", "taipei"]
